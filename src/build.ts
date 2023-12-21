3cfca367be6b3d85a1574930d0c0bcd84dc263ba
raipen <raipen@knu.ac.kr>
2023-12-21 18:13:23 +0900

    feat: make build and devServer command

---
+++ src/build.ts
@@ -0,0 +1,5 @@
+import getPost from './getPost.js';
+
+export default async function build(){
+    getPost();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
-import getPost from './getPost.js';
+import build from './build';
+import devServer from './dev-server';
 
 const args = process.argv.slice(2);
 console.log(args);
-getPost();
+
+if(args[0] === 'build') build();
+else if(args[0] === '') devServer();
+else console.log('Unknown command');
